18140de75c839caf112557220022124defab5478
banyan <ameutau@gmail.com>
2011-05-12 20:08:23 +0900

    /etc/bashrc だけに対応

--- install.sh
+++ install.sh
@@ -13,24 +13,19 @@ current_dir=`pwd`
 echo "Make links for git-bash-completion"
 find $current_dir -name 'git-*' -exec ln -sbi --suffix=.orig {} /usr/local/etc/bash-completion.d/ \;
 
-# change users
-# users=(hoge foo bar)
-
-for user in ${users[@]};do
-    bashrc="/home/${user}/.bashrc"
-    echo "# git completion" >> $bashrc
-    echo "if [ -f /usr/local/etc/bash-completion.d/git-completion.bash ]; then" >> $bashrc
-    echo "    source /usr/local/etc/bash-completion.d/git-completion.bash" >> $bashrc
-    echo "fi" >> $bashrc
-    echo >> $bashrc
-    echo "# git-flow completion" >> $bashrc
-    echo "if [ -f /usr/local/etc/bash-completion.d/git-flow-completion.bash ]; then" >> $bashrc
-    echo "    source /usr/local/etc/bash-completion.d/git-flow-completion.bash" >> $bashrc
-    echo "fi" >> $bashrc
-    echo >> $bashrc
-    echo "# Current Git Branch in Bash Prompt" >> $bashrc
-    echo "parse_git_branch() {" >> $bashrc
-    echo "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(git::\1)/'" >> $bashrc
-    echo "}" >> $bashrc
-    echo "export PS1='\[\033[00m\]\u@\h\[\033[01;34m\] \w \[\033[31m\]\$(parse_git_branch)\[\033[00m\]$\[\033[00m\] '" >> $bashrc
-done
+bashrc="/etc/bashrc"
+echo "# git completion" >> $bashrc
+echo "if [ -f /usr/local/etc/bash-completion.d/git-completion.bash ]; then" >> $bashrc
+echo "    source /usr/local/etc/bash-completion.d/git-completion.bash" >> $bashrc
+echo "fi" >> $bashrc
+echo >> $bashrc
+echo "# git-flow completion" >> $bashrc
+echo "if [ -f /usr/local/etc/bash-completion.d/git-flow-completion.bash ]; then" >> $bashrc
+echo "    source /usr/local/etc/bash-completion.d/git-flow-completion.bash" >> $bashrc
+echo "fi" >> $bashrc
+echo >> $bashrc
+echo "# Current Git Branch in Bash Prompt" >> $bashrc
+echo "parse_git_branch() {" >> $bashrc
+echo "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(git::\1)/'" >> $bashrc
+echo "}" >> $bashrc
+echo "export PS1='\[\033[00m\]\u@\h\[\033[01;34m\] \w \[\033[31m\]\$(parse_git_branch)\[\033[00m\]$\[\033[00m\] '" >> $bashrc
